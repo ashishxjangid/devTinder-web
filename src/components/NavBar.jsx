b56aee87a851c99b30c9f9c37a0d99a1b3170433
ashishxjangid <aj350398@gmail.com>
2025-08-16 16:33:33 +0530

    setup redux store

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
+import {useSelector} from "react-redux";
 
 const NavBar = () => {
+  const user = useSelector((store)=>store.user);
   return (
-    <div className="navbar bg-[#7F1734] shadow-sm">
+    <div className="navbar bg-[#A50044] shadow-sm">
         <div className="flex-1">
           <a className="btn btn-ghost text-xl">DevTinder</a>
         </div>
+        {user && (
         <div className="flex gap-2">
-          
+          <div className="form-control">Welcome, {user.firstName}</div>
           <div className="dropdown dropdown-end mx-5">
             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
                 <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                  alt="user photo"
+                  src={user.photoUrl} />
               </div>
             </div>
             <ul
@@ -30,6 +33,7 @@ const NavBar = () => {
             </ul>
           </div>
         </div>
+        )}
       </div>
   )
 }
